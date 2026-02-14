12194f62200a64086355a50d44c0239e924ecf3b
Pandey <Aditya.Pandey@ellucian.com>
2026-02-14 22:27:15 +0530

    Added Assigment 1

---
+++ greeting.py
@@ -0,0 +1,4 @@
+Fname= input("Enter your First name:")
+Lname= input("Enter your Last name:")
+
+print("Hello,", Fname, Lname, "! Welcome to the Python Program.")
---
+++ math_op.py
@@ -0,0 +1,9 @@
+a= int(input("Enter the first number:"))
+b= int(input("Enter the second number:"))
+
+sum= a+b
+sub= a-b
+multi= a*b  
+div= float(a/b)
+
+print("Addition:", sum, "\nSubtraction :", sub, "\nMultiplication :", multi, "\nDivision :", div)
